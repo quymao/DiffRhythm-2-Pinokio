2453fc9f6c3469ab53383d1b8bf9cba0da24ef15
PierrunoYT <pierrebruno@hotmail.ch>
2026-01-03 13:19:45 +0100

    Remove redundant shell command for git pull from update.js

--- update.js
+++ update.js
@@ -2,11 +2,6 @@ module.exports = {
   run: [{
     method: "shell.run",
     params: {
-      message: "git pull"
-    }
-  }, {
-    method: "shell.run",
-    params: {
       path: "app",
       message: "git pull"
     }
